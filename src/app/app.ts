f86c0a8227dfc56b811e79631164056e9c4a0ab7
Bahiprakash <152366216+Bahiprakash@users.noreply.github.com>
2025-12-24 01:17:49 +0530

    Fix! Updated Left Panel Gradient

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import {NavbarComponent} from './app/navbar/navbar';
 import {LeftPanel} from './app/left-panel/left-panel';
 import {MainContent} from './app/main-content/main-content';
 
 @Component({
   selector: 'app-root',
-             imports: [RouterOutlet, NavbarComponent, LeftPanel, MainContent],
+             imports: [ NavbarComponent, LeftPanel, MainContent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
